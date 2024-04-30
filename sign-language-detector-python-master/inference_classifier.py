6c41179cfaacb99835c3614d913654a56ab41d92
diksha agarwal <113424505+Diiksha-agarwal@users.noreply.github.com>
2024-04-30 19:41:44 +0530

    Update inference_classifier.py
    to match the input format now required due to changes in creation of dataset.

--- sign-language-detector-python-master/inference_classifier.py
+++ sign-language-detector-python-master/inference_classifier.py
@@ -2,9 +2,9 @@ import pickle
 
 import cv2
 import mediapipe as mp
-import numpy as np
+import pandas as pd 
 
-model_dict = pickle.load(open('./model.p', 'rb'))
+model_dict = pickle.load(open('df_model.p', 'rb'))
 model = model_dict['model']
 
 cap = cv2.VideoCapture(0)
@@ -13,17 +13,13 @@ mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 
-hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
+hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3,max_num_hands=1)
 
-labels_dict = {0: 'A', 1: 'B', 2: 'C',3:'D',4:'E',5:'F',6:'G',7:'H',8:'I',9:'J',10:'K',11:'L',12:'M',13:'N',14:'O',15:'P',16:'Q',17:'R',18:'S',19:'T',20:'U',21:'V',22:'W',23:'X',24:'Y',25:'Z'}
+labels_dict = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 'I', 9: 'J', 10: 'K', 11:'L', 12:'M', 13:'N', 14:'O', 15:'P',16:'Q',17:'R',18:'S',19:'T',20:'U', 21:'V', 22:'W', 23:'X',24:'Y',25:'Z',26:'I LOVE YOU',27:'PLEASE',28:'SORRY',29:'THANK YOU',30:'NO',31:'YES',32:'HELLO'}
+
+
 while True:
-
-    data_aux = []
-    x_ = []
-    y_ = []
-
     ret, frame = cap.read()
-
     H, W, _ = frame.shape
 
     frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -39,31 +35,32 @@ while True:
                 mp_drawing_styles.get_default_hand_connections_style())
 
         for hand_landmarks in results.multi_hand_landmarks:
+            data = pd.DataFrame(columns=['x0','x1','x2','x3', 'x4','x5','x6', 'x7','x8','x9','x10','x11','x12','x13','x14','x15','x16','x17','x18','x19','x20', 'y0', 'y1', 'y2', 'y3', 'y4', 'y5', 'y6', 'y7', 'y8', 'y9', 'y10', 'y11', 'y12','y13','y14','y15','y16','y17','y18','y19', 'y20'])
+            x_ = []
+            y_ = []
+            new_row = []
             for i in range(len(hand_landmarks.landmark)):
                 x = hand_landmarks.landmark[i].x
                 y = hand_landmarks.landmark[i].y
-
                 x_.append(x)
                 y_.append(y)
 
             for i in range(len(hand_landmarks.landmark)):
-                x = hand_landmarks.landmark[i].x
-                y = hand_landmarks.landmark[i].y
-                data_aux.append(x - min(x_))
-                data_aux.append(y - min(y_))
+                    x = hand_landmarks.landmark[i].x
+                    y = hand_landmarks.landmark[i].y
+                    new_row.extend([x- min(x_), y - min(y_)])
+                    
+            x1 = int(min(x_) * W) - 10
+            y1 = int(min(y_) * H) - 10
 
-        x1 = int(min(x_) * W) - 10
-        y1 = int(min(y_) * H) - 10
+            x2 = int(max(x_) * W) - 10
+            y2 = int(max(y_) * H) - 10
+            data = data.append(pd.Series(new_row, index=data.columns), ignore_index=True)
+            prediction = model.predict(data)
+            predicted_character = labels_dict[int(prediction[0])]
 
-        x2 = int(max(x_) * W) - 10
-        y2 = int(max(y_) * H) - 10
-
-        prediction = model.predict([np.asarray(data_aux)])
-
-        predicted_character = labels_dict[int(prediction[0])]
-
-        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 0), 4)
-        cv2.putText(frame, predicted_character, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 0), 3,
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 0), 4)
+            cv2.putText(frame, predicted_character, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 0), 3,
                     cv2.LINE_AA)
 
     cv2.imshow('frame', frame)
