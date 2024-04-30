1a79733b65418e7da15eafc5bb4165b196234ad6
diksha agarwal <113424505+Diiksha-agarwal@users.noreply.github.com>
2024-04-30 19:40:07 +0530

    Update train_classifier.py
    for the consequent change in the create dataset.

--- sign-language-detector-python-master/train_classifier.py
+++ sign-language-detector-python-master/train_classifier.py
@@ -1,17 +1,16 @@
-import pickle
-
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-import numpy as np
 
+import pickle
+import pandas as pd
 
-data_dict = pickle.load(open('./data.pickle', 'rb'))
+df = pd.read_csv('keypoints.csv')
 
-data = np.asarray(data_dict['data'])
-labels = np.asarray(data_dict['labels'])
+X = df.drop(columns = 'class')
+y = df['class']
 
-x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, shuffle=True, stratify=labels)
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=True)
 
 model = RandomForestClassifier()
 
@@ -23,6 +22,6 @@ score = accuracy_score(y_predict, y_test)
 
 print('{}% of samples were classified correctly !'.format(score * 100))
 
-f = open('model.p', 'wb')
+f = open('df_model.p', 'wb')
 pickle.dump({'model': model}, f)
 f.close()
