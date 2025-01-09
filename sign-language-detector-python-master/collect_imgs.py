140792566163cd7f7654dfdebfe59e6969f838b9
diksha agarwal <113424505+Diiksha-agarwal@users.noreply.github.com>
2025-01-09 10:46:19 +0530

    Update collect_imgs.py

--- sign-language-detector-python-master/collect_imgs.py
+++ sign-language-detector-python-master/collect_imgs.py
@@ -11,7 +11,7 @@ number_of_classes = 26
 dataset_size = 100
 
 cap = cv2.VideoCapture(0)
-for j in range(20,number_of_classes):
+for j in range(number_of_classes):
     if not os.path.exists(os.path.join(DATA_DIR, str(j))):
         os.makedirs(os.path.join(DATA_DIR, str(j)))
 
